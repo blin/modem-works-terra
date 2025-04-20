f02572589b8c7e6bcb98ef4fe2b128ecabdc733f
Evgenii Petrov <blinunleius@gmail.com>
2025-04-20 16:19:16 +0100

    chore: Remove unused config and image list files

--- Arduino Code/Terra/config.ino
+++
@@ -1,3 +0,0 @@
-// config.ino - Contains trail configuration and device settings
-// NOTE: Most definitions moved to Terra.ino to resolve compilation order issues.
-// This file is now mostly empty but kept for structure. Consider removing or merging later.
